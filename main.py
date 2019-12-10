06bcf78b104598df7ed02e9176fc4642e8509743
Francesco Celenza <checco640@gmail.com>
2019-12-10 14:37:23 +0100

    Modifica main

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ def read_symptoms():
 
         
 
-def main(conn):
+def main():
     
+    conn = SQL.SQLConnect()
     print("Insert your symptoms. Type '/' to stop insertion.")
     list = read_symptoms()
     print(list)
